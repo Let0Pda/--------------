7e106eebef1f0bc8310a40bcd85a8b78cfb3bec5
Let0Pda <a.gavrilov70@gmail.com>
2022-12-04 19:57:05 +0300

    ДЗ4

---
+++ DZ4/db.sql
@@ -0,0 +1,13 @@
+CREATE TABLE students ( 
+  id INTEGER PRIMARY KEY, 
+  name TEXT NOT NULL, 
+  age INTEGER,
+  address TEXT
+); 
+
+INSERT INTO students VALUES (01, 'Алексей Гаврилов', 55, 'Москва');
+INSERT INTO students VALUES (02, "Иван Иванов", 25, 'Нижний Новгород'); 
+INSERT INTO students VALUES (03, "Денис Денисов", 37, 'Новосибирск'); 
+INSERT INTO students VALUES (04, "Ирина Иванова", 48, 'Рязань'); 
+
+
